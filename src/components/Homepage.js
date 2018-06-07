ab18ba48294e27f6cc80d9c8330c113c1119012d
Aleya Chowdhury <caleya91@gmail.com>
2018-06-07 14:28:04 -0400

    added functionality ASN page

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -4,69 +4,87 @@ import axios from 'axios';
 import { FormGroup, Col, Panel, Radio } from 'react-bootstrap';
 import $ from 'jquery';
 import NavigationBar from './NavigationBar';
-
+import { url } from '../config';
 class Homepage extends Component{
 
 
 	constructor(props){
 		super(props);
 		this.state ={
+			asns: []
           
 		}
 	}
+
+	componentDidMount() {
+    axios.get(`${url}/api/asns`)
+         .then(res => res.data)
+         .then(
+           (data) => {  
+             this.setState({
+                 asns: data
+              });
+            },
+            (error) => {
+              this.setState({
+                asns: []
+              })
+            }
+          ) 
+}
+
+		
 		
 	
 
 	render() {
 		document.querySelector('body').style.backgroundColor = "white";
-          
-		    return (
 
-		      <div>
-		        
-
-		         <Panel id="collapsible-panel-example-3">
+		console.log(this.state.asns);
+	   const asnReturn = this.state.asns.map((aASN,index)=>{
+		          return( <Panel id="collapsible-panel-example-3">
 		          <Panel.Heading>
 		            <Panel.Title>ASN Number</Panel.Title>
-		            <Panel.Toggle componentClass="a">ASN</Panel.Toggle>
+		            <Panel.Toggle componentClass="a">{aASN.asn}</Panel.Toggle>
 		          </Panel.Heading>
+
 		          <Panel.Collapse>
 		            <Panel.Body>
-		                  <ul> 
-		                       <li>Asn Id: </li>
-		                       <li>Vendor id: </li>
-		                       <li>Expected Arrival Date: </li>
-		                        <li> Expect Arriaval Time: </li>
-		                        <li> Serial Number : [] </li>
-		                       </ul>
+		                   
+                               <ul> 
+		                     
+		                      
+		                       <li>Expected Arrival Date:{aASN.expectedArrivalDate} </li>
+		                       <li>Expect Arriaval Time: {aASN.expectedArrivalTime}</li>
+		                       <li>Serial Number:  {aASN.serials.map((aSerial, index)=>{
+		                       	         return(<li>{aSerial.serial}</li>)
+		                       })
 
+                                  }
+		                         </li>
+		                       </ul>
+		                    
+		               
 		            </Panel.Body>
 		          </Panel.Collapse>
-		          </Panel>
-		           <Panel id="collapsible-panel-example-3">
-		           <Panel.Heading>
-		            <Panel.Title>ASN Number</Panel.Title>
-		            <Panel.Toggle componentClass="a">ASN</Panel.Toggle>
-		          </Panel.Heading>
-		          <Panel.Collapse>
-		            <Panel.Body>
-		                  <ul> 
-		                       <li>Asn Id: </li>
-		                       <li>Vendor id: </li>
-		                       <li>Expected Arrival Date: </li>
-		                        <li> Expect Arriaval Time: </li>
-		                        <li> Serial Number : [] </li>
-		                       </ul>
+		          </Panel>)
+	      
+	     })
 
-		            </Panel.Body>
-		          </Panel.Collapse>
-		        </Panel>
-		        </div>
-		        )
+          
+		    return (
+		    	   
+                              
+		      <div>
+                   {asnReturn}
+		          
+		        </div>)
+		        
+		        }
 }
 
 
-}
+
 
 export default Homepage;
 
